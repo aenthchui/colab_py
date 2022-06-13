1654a814d47190e7c0fd04aa24fa664f5e88f569
aenthchui <96768439+aenthchui@users.noreply.github.com>
2022-06-13 16:59:43 +0800

    Create background.py

---
+++ background.py
@@ -0,0 +1,12 @@
+import shutil
+labels=[]
+lines=[]
+fileRead = False
+
+def unzipFiles():
+    shutil.unpack_archive("converted_keras.zip", "converted_keras")
+    with open('./converted_keras/labels.txt') as f:
+        lines = f.readlines()
+    f.close()
+    for x in lines:
+            labels.append(x[x.find(' ')+1:len(x)-1])
